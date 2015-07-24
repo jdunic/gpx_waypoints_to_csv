215886f1d9cebdf550a6b667d5b109207bc43afe
J Dunic <jdunic@gmail.com>
2015-07-23 20:11:13 -0400

    add user input data and functionality for conversion of a single gpx file

--- gpx_to_csv_function.py
+++ gpx_to_csv_function.py
@@ -13,13 +13,50 @@
 #Extract GPX file data into csv
 
 # Load libraries
+import distutils.core
+import os
+
 import gpxpy
 
 import csv
 import time
 from datetime import date
 
-def gpx_to_csv(output_name, gpx_filenames = None):
+# Get input and output file names and locations
+def get_input_data():
+    # User prompts and confirmation that information entered is correct. In case of an error, type 'exit' to escape.
+    print('Type "exit" to escape')
+    username = raw_input("Enter your name: ")
+    username = username.rstrip()
+#
+    if username == 'exit':
+        return None
+    gpx_filename = raw_input("Enter the gpx file path: ")
+    gpx_filename = gpx_filename.rstrip()
+#    
+    assert os.path.exists(gpx_filename), "I did not find the file at, "+str(gpx_filename)
+#
+    if gpx_filename == 'exit':
+        return None
+#
+    output_filename = raw_input("Enter the output file path; e.g., PATH/filename1.csv: ")
+    output_filename = output_filename.rstrip()
+#
+    if output_filename == 'exit':
+        return None
+#   
+    # Print confirmation message
+    print("Your name is:", username, "Your input file path is:", gpx_filename, "and your output file path", output_filename)
+    confirmation = raw_input("Is this information correct? If no, you will be able to re-enter this information. (y/n) ")
+    go_ahead = distutils.util.strtobool(confirmation)
+#
+    names = ('username', 'gpx_filename', 'output_filename', 'confirmation')
+    inputs = (username, gpx_filename, output_filename, confirmation)
+    info_dict = dict(zip(names, inputs))
+#
+    return(info_dict)
+
+def gpx_to_csv(output_name, gpx_filenames):
     if isinstance(gpx_filenames, list) is False:
         gpx_filenames = [gpx_filenames] 
     wp_list = []
@@ -36,7 +73,7 @@ def gpx_to_csv(output_name, gpx_filenames = None):
                 'time': w.time.strftime("%H:%M:%S")
             }
             wp_list.append(wp_dict)
-    return(wp_list)
+    #return(wp_list)
 
     keys = wp_list[0].keys()
     with open(output_name, 'w') as f:
@@ -44,3 +81,9 @@ def gpx_to_csv(output_name, gpx_filenames = None):
         writer.writeheader()
         writer.writerows(wp_list)
 
+
+# For a single file
+input_data = get_input_data()
+
+gpx_to_csv(output_name = input_data['output_filename'], 
+           gpx_filenames = input_data['gpx_filename'])
